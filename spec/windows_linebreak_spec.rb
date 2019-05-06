74e77984422b41d6c89dc6d3a538bc264752c0b6
Philippe Hardardt <phil.henri@gmail.com>
2019-05-05 22:54:42 -0300

    Fix path and filename.

--- spec/windows_linebreak_spec.rb
+++ spec/windows_linebreak_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 RSpec.describe "Windows Linebreak", type: :aruba do
   it "compares linebreaks in Windows" do
     if Gem.win_platform?
-      run_command(%{put_string.bat})
+      run_command(%{bin/put_string.bat})
     else
-      run_command(%{put_string})
+      run_command(%{bin/put_string})
     end
 
     expect(last_command_stopped.output).to include("a\nb")
