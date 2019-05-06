ec98c57d2c7808a36e30598029fc70d8baeebf13
Philippe Hardardt <phil.henri@gmail.com>
2019-05-05 22:59:15 -0300

    Remove unecessary condition.

--- spec/windows_linebreak_spec.rb
+++ spec/windows_linebreak_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 
 RSpec.describe "Windows Linebreak", type: :aruba do
   it "compares linebreaks in Windows" do
-    if Gem.win_platform?
-      run_command(%{bin/put_string.bat})
-    else
-      run_command(%{bin/put_string})
-    end
+    run_command(%{bin/put_string})
 
     expect(last_command_stopped.output).to include("a\nb")
   end
